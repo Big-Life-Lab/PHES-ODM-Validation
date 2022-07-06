12fa5a6b8a4f7e92fca99f711d1d8641d2260f70
zargot <zargot.ripley@gmail.com>
2022-07-06 15:56:34 -0400

    utils: refactor

--- utils.py
+++ utils.py
@@ -3,24 +3,32 @@ import csv
 # TODO: document requirement
 from xlsx2csv import Xlsx2csv
 
-# TODO: dynamic input filename
+
+def convertXlsx2Csv(xlsxFile, sheetName, csvFile: str):
+    wb = Xlsx2csv(xlsxFile, outputencoding="utf-8")
+    ws = wb.getSheetIdByName(sheetName)
+    wb.convert(csvFile, ws)
+
+
+def importCsvFile(fileName: str) -> (dict, list):
+    """Return (cols, rows)"""
+    cols = {}
+    rows = []
+    with open(fileName, newline='') as f:
+        r = csv.reader(f)
+        headers = next(r)
+        cols = dict(zip(headers, range(len(headers))))
+        for row in r:
+            rows.append(row)
+    return (cols, rows)
+
+
+sheetName = "parts"
+xlsxFile = "data.xlsx"
 csvFile = "data.csv"
-excelFile = "data.xlsx"
 
-# convert excel to csv
-wb = Xlsx2csv(excelFile, outputencoding="utf-8")
-ws = wb.getSheetIdByName("parts")
-wb.convert(csvFile, ws)
-
-# import csv
-cols = {}
-rows = []
-with open(csvFile, newline='') as f:
-    r = csv.reader(f)
-    headers = next(r)
-    cols = dict(zip(headers, range(len(headers))))
-    for row in r:
-        rows.append(row)
+convertXlsx2Csv(xlsxFile, sheetName, csvFile)
+(cols, rows) = importCsvFile(csvFile)
 
 partId = cols["partID"]
 for row in rows:
