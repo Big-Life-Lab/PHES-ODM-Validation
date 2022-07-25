668d4141eeac783417db81827fef7f26fa645604
zargot <zargot.ripley@gmail.com>
2022-07-25 17:00:59 -0400

    add test code

--- src/odm_validation/main.py
+++ src/odm_validation/main.py
@@ -24,3 +24,73 @@ parts = fetchParts()
 schema = validate.generate_cerberus_schema(parts)
 pp = pprint.PrettyPrinter(width=80, compact=True)
 pp.pprint(schema)
+
+schema = {
+    "addresses": {
+        "type": "list",
+        "schema": {
+            "type": "dict",
+            "schema": {
+                "addressID": {
+                    "required": True,
+                    "meta": {
+                        "partID": "addressID",
+                        "addresses": "PK",
+                        "addressesRequired": "mandatory",
+                    }
+                },
+                "addL2": {
+                    "meta": {
+                        "partID": "contactID",
+                        "contacts": "PK",
+                        "contactsRequired": "NA"
+                    }
+                }
+            },
+            "meta": {
+                "partID": "addresses",
+                "partType": "table"
+            }
+        }
+    },
+    "contacts": {
+        "type": "list",
+        "schema": {
+            "type": "dict",
+            "schema": {
+                "contactID": {
+                    "required": True,
+                    "meta": {
+                        "partID": "contactID",
+                        "ContactTable": "PK",
+                        "ContactTableRequired": "mandatory"
+                    }
+                }
+            }
+        },
+        "meta": {
+            "partID": "contacts",
+            "partType": "table"
+        }
+    }
+}
+
+data = {
+    "addresses": [
+        {
+            "addressID": "1",
+        },
+        {
+            "addL2": "12345 Lane Avenue"
+        }
+    ],
+    "contacts": [
+        {
+            "contactID": "1"
+        }
+    ]
+}
+
+print()
+errors = validate.validate_data(schema, data)
+pp.pprint(errors)
