8369fa69717c1647b28ae0070f5d4b172bf21638
zargot <zargot.ripley@gmail.com>
2022-09-21 12:46:28 -0400

    tools: add generate_schemas.py

---
+++ tools/generate_schemas.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import logging
+import os
+import re
+import sys
+from os.path import join, normpath
+from pathlib import Path
+
+root_dir = join(os.path.dirname(os.path.realpath(__file__)), '..')
+sys.path.append(join(root_dir, 'src'))
+
+import odm_validation.utils as utils  # noqa:E402
+from odm_validation.validation import generate_validation_schema  # noqa:E402
+from odm_validation.versions import parse_version  # noqa:E402
+
+PARTS_FILENAME = 'parts.csv'
+
+# setup default logger (to file)
+log_dir = normpath(join(root_dir, 'logs'))
+log_file = join(log_dir, 'generate_schemas.log')
+logging.basicConfig(
+    level=logging.DEBUG,
+    handlers=[
+        logging.FileHandler(log_file)
+    ],
+)
+
+
+def info(x):
+    print(x)
+    logging.info(x)
+
+
+def warning(x):
+    print(x)
+    logging.warning(x)
+
+
+def main():
+    dir = os.path.dirname(os.path.realpath(__file__))
+    asset_dir = join(dir, '../assets')
+    schema_dir = normpath(join(asset_dir, 'validation-schemas'))
+    dataset_dir = normpath(join(asset_dir, 'dictionary'))
+
+    info(f'looking for parts in {dataset_dir}')
+    p = Path(dataset_dir)
+    sources = list(p.glob(f'v*/{PARTS_FILENAME}'))
+    if len(sources) == 0:
+        warning('no files found')
+    info(f'writing schemas to {schema_dir}')
+
+    for parts_file in sources:
+        parts_filename = os.path.basename(parts_file)
+        version_regex = f'.+/v(.+)/{PARTS_FILENAME}'
+        if not (match := re.search(version_regex, str(parts_file))):
+            continue
+        version = str(parse_version(match.group(1)))
+        schema_filename = f'schema-v{version}.yml'
+        info(f'converting {parts_filename} --> {schema_filename}')
+        parts = utils.import_dataset(parts_file)
+        schema = generate_validation_schema(parts, version)
+        path = join(schema_dir, schema_filename)
+        utils.export_schema(schema, path)
+    info('done')
+
+
+if __name__ == "__main__":
+    main()
