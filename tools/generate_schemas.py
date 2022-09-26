e314bbb966384125fdb38ef23147f9ec757db29a
zargot <zargot.ripley@gmail.com>
2022-09-26 11:56:14 -0400

    tools: fix logging

--- tools/generate_schemas.py
+++ tools/generate_schemas.py
@@ -14,37 +14,40 @@ import odm_validation.utils as utils  # noqa:E402
 from odm_validation.validation import generate_validation_schema  # noqa:E402
 from odm_validation.versions import parse_version  # noqa:E402
 
-log_dir = join(root_dir, 'logs')
+
+# setup default logger (to file)
+log_dir = normpath(join(root_dir, 'logs'))
 log_file = join(log_dir, 'generate_schemas.log')
+logging.basicConfig(
+    level=logging.DEBUG,
+    handlers=[
+        logging.FileHandler(log_file)
+    ],
+)
 
 
-def setup_logging():
-    logging.basicConfig(level=logging.ERROR)
+def info(x):
+    print(x)
+    logging.info(x)
 
-    logger = logging.getLogger('my_logger')
-    logger.setLevel(logging.DEBUG)
 
-    print(f'logging to {log_file}')
-    os.makedirs(log_dir, exist_ok=True)
-    fh = logging.FileHandler(log_file)
-    fh.setLevel(logging.DEBUG)
-    logger.addHandler(fh)
+def warning(x):
+    print(x)
+    logging.warning(x)
 
 
 def main():
-    setup_logging()
-
     dir = os.path.dirname(os.path.realpath(__file__))
     asset_dir = join(dir, '../assets')
     schema_dir = normpath(join(asset_dir, 'validation-schemas'))
     dataset_dir = normpath(join(asset_dir, 'dictionary'))
 
-    print(f'looking for parts in {dataset_dir}')
+    info(f'looking for parts in {dataset_dir}')
     p = Path(dataset_dir)
     sources = list(p.glob('v*/parts.csv'))
     if len(sources) == 0:
-        print('no files found')
-    print(f'writing schemas to {schema_dir}')
+        warning('no files found')
+    info(f'writing schemas to {schema_dir}')
 
     for parts_file in sources:
         parts_filename = os.path.basename(parts_file)
@@ -53,12 +56,12 @@ def main():
         v = match.group(1)
         version = str(parse_version(v))
         schema_filename = f'schema-v{version}.yml'
-        print(f'converting {parts_filename} --> {schema_filename}')
+        info(f'converting {parts_filename} --> {schema_filename}')
         parts = utils.import_dataset(parts_file)
         schema = generate_validation_schema(parts, version)
         path = join(schema_dir, schema_filename)
         utils.export_schema(schema, path)
-    print('done')
+    info('done')
 
 
 if __name__ == "__main__":
