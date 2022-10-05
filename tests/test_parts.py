5c35fef7b0533a55dee07744dbaf0e61c728b407
zargot <zargot.ripley@gmail.com>
2022-10-05 14:33:48 -0400

    fix test

--- tests/test_parts.py
+++ tests/test_parts.py
@@ -2,6 +2,7 @@ import unittest
 
 import context
 from part_tables import is_compatible
+from versions import parse_version
 
 context.unused_import_dummy = 1
 
@@ -14,22 +15,22 @@ def get_row(first: str, last: str, active: bool):
     }
 
 
-class TestValidateVersion(unittest.TestCase):
+class TestVersionCompat(unittest.TestCase):
     def test_same(self):
         row = get_row('1.0.0', '1.0.0', True)
-        self.assertTrue(is_compatible(row, '1.0.0'))
+        self.assertTrue(is_compatible(row, parse_version('1.0.0')))
 
     def test_eq_first(self):
         row = get_row('1.0.0', '2.0.0', False)
-        self.assertTrue(is_compatible(row, '1.0.0'))
+        self.assertTrue(is_compatible(row, parse_version('1.0.0')))
 
     def test_eq_last(self):
         row = get_row('1.0.0', '2.0.0', False)
-        self.assertFalse(is_compatible(row, '2.0.0'))
+        self.assertFalse(is_compatible(row, parse_version('2.0.0')))
 
     def test_incomplete(self):
         row = get_row('2', '2.0', True)
-        self.assertTrue(is_compatible(row, '2.0.'))
+        self.assertTrue(is_compatible(row, parse_version('2.0.')))
 
 
 if __name__ == '__main__':
