edb4f9c2c67b7bc17a95f6670f9408c0f41a91ad
zargot <zargot.ripley@gmail.com>
2023-02-06 10:35:05 -0500

    tools/schema_gen: add gen of v1 schemas

--- tools/generate_schemas.py
+++ tools/generate_schemas.py
@@ -4,8 +4,9 @@ import logging
 import os
 import re
 import sys
-from os.path import join, normpath
+from os.path import join, normpath, relpath
 from pathlib import Path
+from semver import Version
 
 root_dir = join(os.path.dirname(os.path.realpath(__file__)), '..')
 sys.path.append(join(root_dir, 'src'))
@@ -15,6 +16,10 @@ from odm_validation.validation import generate_validation_schema  # noqa:E402
 from odm_validation.versions import parse_version  # noqa:E402
 
 PARTS_FILENAME = 'parts.csv'
+LEGACY_SCHEMA_VERSIONS = sorted([
+    Version(major=1),
+    Version(major=1, minor=1),
+])
 
 # setup default logger (to file)
 log_dir = normpath(join(root_dir, 'logs'))
@@ -37,6 +42,26 @@ def warning(x):
     logging.warning(x)
 
 
+def generate_schema_for_version(parts, version, schema_dir):
+    filename = f'schema-v{version}.yml'
+    path = join(schema_dir, filename)
+    info(f'generating {os.path.basename(path)}')
+    schema = generate_validation_schema(parts, version)
+    utils.export_schema(schema, path)
+
+
+def generate_schemas_from_parts(parts_path, schema_dir):
+    info(f'using {relpath(parts_path)}')
+    parts_path_regex = f'.+/v(.+)/{PARTS_FILENAME}'
+    if not (match := re.search(parts_path_regex, str(parts_path))):
+        return
+    parts = utils.import_dataset(parts_path)
+    parts_version = parse_version(match.group(1))
+    for version in (LEGACY_SCHEMA_VERSIONS + [parts_version]):
+        assert version <= parts_version
+        generate_schema_for_version(parts, str(version), schema_dir)
+
+
 def main():
     dir = os.path.dirname(os.path.realpath(__file__))
     asset_dir = join(dir, '../assets')
@@ -52,17 +77,7 @@ def main():
         info(f'writing schemas to {schema_dir}')
 
     for parts_path in parts_paths:
-        parts_filename = os.path.basename(parts_path)
-        parts_path_regex = f'.+/v(.+)/{PARTS_FILENAME}'
-        if not (match := re.search(parts_path_regex, str(parts_path))):
-            continue
-        version = str(parse_version(match.group(1)))
-        schema_filename = f'schema-v{version}.yml'
-        info(f'converting {parts_filename} --> {schema_filename}')
-        parts = utils.import_dataset(parts_path)
-        schema = generate_validation_schema(parts, version)
-        path = join(schema_dir, schema_filename)
-        utils.export_schema(schema, path)
+        generate_schemas_from_parts(parts_path, schema_dir)
     info('done')
 
 
