7aeefd9bd7aada0eebc89a15ac6ef9ace8b637ac
zargot <zargot.ripley@gmail.com>
2023-02-08 14:13:22 -0500

    add test for min/max-value datetime rule
    - move error-report-4 to error-report-pass-2
    - add error-report-4 and error-report-pass-4 for datetime

--- tests/test_min_max_value.py
+++ tests/test_min_max_value.py
@@ -6,11 +6,11 @@ from parameterized import parameterized, parameterized_class
 
 import common
 from common import asset, root_dir, param_range
+from stdext import deep_update
 from utils import (
     import_dataset,
     import_json_file,
     import_schema,
-    import_yaml_file
 )
 from validation import _generate_validation_schema_ext, _validate_data_ext
 
@@ -21,36 +21,38 @@ class Assets():
         common.ASSET_DIR = join(root_dir,
                                 f'assets/validation-rules/{rule_dirname}')
 
-        self.parts_v2 = import_dataset(asset('parts.csv'))
+        datetime_parts = import_dataset(asset('parts-datetime.csv'))
+        datetime_schema_v1 = import_schema(asset('schema-datetime-v1.yml'))
+        datetime_schema_v2 = import_schema(asset('schema-datetime-v2.yml'))
+
+        self.parts = (import_dataset(asset('parts.csv')) + datetime_parts)
         self.schemas = {
             1: import_schema(asset('schema-v1.yml')),
             2: import_schema(asset('schema-v2.yml')),
         }
+        deep_update(self.schemas[1], datetime_schema_v1)
+        deep_update(self.schemas[2], datetime_schema_v2)
 
-        self.data_pass_v2 = [
-            {'sites': import_dataset(asset('valid-dataset-1.yml'))},
-            {'sites': import_dataset(asset('valid-dataset-2.csv'))},
-            {'sites': import_dataset(asset('valid-dataset-3.yml'))},
-        ]
-
-        self.data_fail_v2 = [
-            {'sites': import_yaml_file(asset('invalid-dataset-1.yml'))},
-            {'sites': import_dataset(asset('invalid-dataset-2.csv'))},
-            {'sites': import_dataset(asset('invalid-dataset-3.csv'))},
-        ]
-
-        self.error_report_fail = [
-            import_json_file(asset('error-report-1.json')),
-            import_json_file(asset('error-report-2.json')),
-            import_json_file(asset('error-report-3.json')),
-        ]
-
+        self.data_pass = []
+        for i in range(1, 5):
+            self.data_pass.append({
+                'sites': import_dataset(asset(f'valid-dataset-{i}.*'))
+            })
         empty_report = {'errors': [], 'warnings': []}
         self.error_report_pass = [
             empty_report,
-            import_json_file(asset('error-report-4.json')),
+            import_json_file(asset('error-report-pass-2.json')),
             empty_report,
+            import_json_file(asset('error-report-pass-4.json')),
         ]
+
+        self.data_fail = []
+        self.error_report_fail = []
+        for i in range(1, 5):
+            self.data_fail.append(
+                {'sites': import_dataset(asset(f'invalid-dataset-{i}.*'))})
+            self.error_report_fail.append(
+                import_json_file(asset(f'error-report-{i}.json')))
 
 
 @parameterized_class([
@@ -69,23 +71,23 @@ class TestMinMaxValue(common.OdmTestCase):
     @parameterized.expand(param_range(1, 3))
     def test_schema_generation(self, major_ver):
         result = _generate_validation_schema_ext(
-            parts=self.assets.parts_v2,
+            parts=self.assets.parts,
             schema_version=f'{major_ver}.0.0',
             rule_whitelist=[self.ruleId])
         self.assertDictEqual(self.assets.schemas[major_ver], result)
 
-    @parameterized.expand(param_range(0, 3))
+    @parameterized.expand(param_range(0, 4))
     def test_passing_datasets(self, ix):
         report = _validate_data_ext(self.assets.schemas[2],
-                                    self.assets.data_pass_v2[ix],
+                                    self.assets.data_pass[ix],
                                     rule_whitelist=self.whitelist)
         expected = self.assets.error_report_pass[ix]
         self.assertReportEqual(expected, report)
 
-    @parameterized.expand(param_range(0, 3))
+    @parameterized.expand(param_range(0, 4))
     def test_failing_datasets(self, ix):
         report = _validate_data_ext(self.assets.schemas[2],
-                                    self.assets.data_fail_v2[ix],
+                                    self.assets.data_fail[ix],
                                     rule_whitelist=self.whitelist)
         expected = self.assets.error_report_fail[ix]
         self.assertReportEqual(expected, report)
