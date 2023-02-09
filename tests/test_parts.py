715fda3e81dbf22fd6e6576d5ee27378174ecfce
zargot <zargot.ripley@gmail.com>
2023-02-09 17:59:52 -0500

    tests: add test for version1table

--- tests/test_parts.py
+++ tests/test_parts.py
@@ -3,7 +3,9 @@ import unittest
 import common
 import part_tables as pt
 from part_tables import get_mappings
+from validation import generate_validation_schema
 from versions import Version, parse_version
+# from pprint import pprint
 
 common.unused_import_dummy = 1
 
@@ -74,6 +76,87 @@ class TestVersion1FieldsExist(common.OdmTestCase):
         id_list_fail = [get_mappings(p, v) for p in self.parts_fail]
         self.assertEqual(id_list_pass, [['a'], ['b'], ['c', 'd']])
         self.assertEqual(id_list_fail, [None, None, None])
+
+
+class TestVersion1Table(common.OdmTestCase):
+    parts = [
+        {
+            'partID': 'mynewtable',
+            'partType': 'table',
+            'version1Table': 'myoldtable',
+            'version1Location': 'tables',
+            'status': 'active',
+        },
+        {
+            'partID': 'mynewtable2',
+            'partType': 'table',
+            'version1Table': 'myoldtable2',
+            'version1Location': 'tables',
+            'status': 'active',
+        },
+        {
+            'partID': 'myoldpart',
+            'partType': 'attribute',
+            'dataType': 'integer',
+            'firstReleased': '1',
+            'lastUpdated': '2',
+            'status': 'depreciated',
+            'version1Table': 'myoldtable;myoldtable2',
+            'version1Location': 'variables',
+            'version1Variable': 'myoldpart',
+        }
+    ]
+
+    expected_schema = {
+        'myoldtable': {
+            'type': 'list',
+            'schema': {
+                'type': 'dict',
+                'schema': {'myoldpart': {
+                    'type': 'integer',
+                    'coerce': 'integer',
+                    'meta': [{
+                        'meta': [{
+                            'partID': 'myoldpart',
+                            'dataType': 'integer',
+                            'version1Location': 'variables',
+                            'version1Table': 'myoldtable;myoldtable2',
+                            'version1Variable': 'myoldpart'
+                            }],
+                        'ruleID': 'invalid_type'}],
+                }},
+                'meta': [{
+                    'partID': 'mynewtable',
+                    'partType': 'table',
+                    'version1Location': 'tables',
+                    'version1Table': 'myoldtable'}]}},
+        'myoldtable2': {
+            'type': 'list',
+            'schema': {
+                'type': 'dict',
+                'schema': {'myoldpart': {
+                    'type': 'integer',
+                    'coerce': 'integer',
+                    'meta': [{
+                        'meta': [{
+                            'partID': 'myoldpart',
+                            'dataType': 'integer',
+                            'version1Location': 'variables',
+                            'version1Table': 'myoldtable;myoldtable2',
+                            'version1Variable': 'myoldpart'
+                            }],
+                        'ruleID': 'invalid_type'}],
+                }},
+                'meta': [{
+                    'partID': 'mynewtable2',
+                    'partType': 'table',
+                    'version1Location': 'tables',
+                    'version1Table': 'myoldtable2'}]}}}
+
+    def test(self):
+        self.maxDiff = None
+        schema = generate_validation_schema(self.parts, '1.0.0')
+        self.assertDictEqual(self.expected_schema, schema['schema'])
 
 
 if __name__ == '__main__':
