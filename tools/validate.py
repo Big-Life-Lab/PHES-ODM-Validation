fead3ebb00ac7610009991173c96611e50383331
zargot <zargot.ripley@gmail.com>
2023-05-30 10:31:05 -0400

    tools/validate: rewrite using new spec
    - assets:
        - update error messages
    - reports:
        - add join_reports
        - rewrite error templates with multiple verbosity levels
    - validation:
        - add verbosity level constant
    - tests:
        - update error messages
    - validate:
        - rewrite

--- tools/validate.py
+++ tools/validate.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
+import json
 import os
 import sys
 import tempfile
+from enum import Enum
 from math import ceil
 from os.path import basename, join, normpath, splitext
 from pathlib import Path
-from typing import List, Optional
+from typing import Dict, List, Optional
 # from pprint import pprint
 
+import jsons
 import typer
+import yaml
 from xlsx2csv import Xlsx2csv
 
 root_dir = join(os.path.dirname(os.path.realpath(__file__)), '..')
@@ -17,21 +21,51 @@ sys.path.append(join(root_dir, 'src'))
 
 import odm_validation.part_tables as pt  # noqa:E402
 import odm_validation.utils as utils  # noqa:E402
-from odm_validation.reports import ErrorKind  # noqa:E402
+import odm_validation.validation as validation  # noqa:E402
+from odm_validation.reports import ErrorKind, join_reports  # noqa:E402
 from odm_validation.validation import _validate_data_ext, DataKind  # noqa:E402
+
+
+class DataFormat(Enum):
+    CSV = 'csv'
+    XLSX = 'xlsx'
+
+
+class Format(Enum):
+    """Output format"""
+    TXT = 'txt'
+    JSON = 'json'
+    YAML = 'yaml'
+
+
+DEF_VER = pt.ODM_VERSION_STR
+
+DATA_FILE_DESC = "Path of input file (xlsx/csv)."
+VERSION_DESC = "ODM version to validate against."
+OUT_DESC = "Output path of validation report. Defaults to stdout/console."
+FORMAT_DESC = "Output format. Defaults to txt if unable to autodetect."
+VERB_DESC = "Error message verbosity, between 0 and 2."
+
+
+def info(s="", line=True):
+    file = sys.stderr
+    if line:
+        print(s, file=file)
+    else:
+        print(s, file=file, end='', flush=True)
 
 
 def import_xlsx(src_file, dst_dir) -> List[str]:
     "Returns list of imported csv files."
     result = []
-    print(f'importing {basename(src_file)}')
+    info(f'importing {basename(src_file)}')
     os.makedirs(dst_dir, exist_ok=True)
     xl = Xlsx2csv(src_file, skip_empty_lines=True)
     for sheet in xl.workbook.sheets:
         name = sheet['name']
         csv_path = join(dst_dir, name + '.csv')
         sheet_id = xl.getSheetIdByName(name)
-        print(f'converting sheet {name}...', flush=True)
+        info(f'converting sheet {name}...')
         xl.convert(csv_path, sheet_id)
         result.append(csv_path)
     return result
@@ -44,18 +78,41 @@ def get_sheet_table_id(schema, sheet_name) -> Optional[str]:
             return table_id
 
 
-def write_results(report, outdir: str, name: str):
-    entries = {
-        ErrorKind.WARNING: report.warnings,
-        ErrorKind.ERROR: report.errors,
+def write_txt_report(output, report):
+    def get_msg(e) -> str:
+        return e['message']
+    errorkind_messages = {
+        ErrorKind.ERROR: list(map(get_msg, report.errors)),
+        ErrorKind.WARNING: list(map(get_msg, report.warnings))
     }
-    for kind in ErrorKind:
-        messages = list(map(lambda x: x['message'], entries[kind]))
-        if len(messages) == 0:
+    for error_kind, messages in errorkind_messages.items():
+        error_name = error_kind.value.capitalize()
+        n = len(messages)
+        output.write(f'{error_name}s: {n}\n')
+        if n == 0:
             continue
-        outfile = os.path.join(outdir, name + f'-{kind.value}s.txt')
-        with open(outfile, 'w') as f:
-            f.write('\n'.join(messages))
+        output.write(('-' * 79) + '\n')
+        output.write('\n'.join(messages) + '\n\n')
+
+
+def write_json_report(output, report):
+    # XXX: serialization with 'jsons' is needed to avoid writing serialization
+    # methods for ValidationReport and TableInfo
+    data = jsons.dump(report)
+    json.dump(data, output)
+
+
+def write_yaml_report(output, report):
+    yaml.dump(report, output)
+
+
+def write_report(output, report, fmt: Format):
+    if fmt == Format.TXT:
+        write_txt_report(output, report)
+    elif fmt == Format.JSON:
+        write_json_report(output, report)
+    elif fmt == Format.YAML:
+        write_yaml_report(output, report)
 
 
 def filename_without_ext(path):
@@ -64,55 +121,180 @@ def filename_without_ext(path):
 
 def on_progress(action, table_id, offset, total):
     percent = int(ceil(offset/total * 100))
-    print('\r' + f'{action}: {percent}%', end='', flush=True)
-    if offset == total:
-        print()
+    info('\r' + f'# {table_id:20} \t{action} {percent}%', line=False)
 
 
-DEF_VER = pt.ODM_VERSION_STR
-
-XLSX_FILE_DESC = "path to input file (excel/xlsx)"
-VERSION_DESC = "ODM version to validate against"
-OUTDIR_DESC = "Output directory for generated files"
+def enum_values(E) -> List[str]:
+    return list(map(lambda e: e.value, iter(E)))
 
 
-def main(xlsx_file: str = typer.Argument(..., help=XLSX_FILE_DESC),
-         version: str = typer.Option(default=DEF_VER, help=VERSION_DESC),
-         outdir: str = typer.Option(default="", help=OUTDIR_DESC)):
+def get_ext(path: Optional[str]) -> str:
+    """returns path file extension without dot"""
+    if not path:
+        return
+    ext = os.path.splitext(path)[1]
+    if len(ext) > 1:
+        return ext[1:]
 
-    if not outdir:
-        outdir = tempfile.mkdtemp(suffix='-'+filename_without_ext(xlsx_file))
-    csvdir = join(outdir, 'csv-files')
-    print(f'writing files to {outdir}\n')
 
-    csv_files = import_xlsx(xlsx_file, csvdir)
+def detect_data_format(path: str) -> Optional[DataFormat]:
+    ext = get_ext(path)
+    try:
+        return DataFormat[ext.upper()]
+    except KeyError:
+        return
 
-    dir = os.path.dirname(os.path.realpath(__file__))
-    asset_dir = join(dir, '../assets')
+
+def detect_output_format(path: Optional[str]) -> Optional[Format]:
+    ext = get_ext(path)
+    try:
+        return Format[ext.upper()]
+    except KeyError:
+        if ext.lower() == 'yml':
+            return Format.YAML
+
+
+def get_schema_path(version: str) -> str:
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    asset_dir = join(script_dir, '../assets')
     schema_dir = Path(normpath(join(asset_dir, 'validation-schemas')))
-    schema_file = join(schema_dir, f'schema-v{version}.yml')
-    schema = utils.import_schema(schema_file)
+    schema_filename = f'schema-v{version}.yml'
+    return join(schema_dir, schema_filename)
 
-    for file in csv_files:
-        name = filename_without_ext(file)
-        print(f'\nvalidating sheet "{name}" ', end='', flush=True)
-        data = utils.import_dataset(file)
+
+def preprocess_datafile(path: str, fmt: DataFormat) -> List[str]:
+    """Returns data paths."""
+    result = []
+    if fmt == DataFormat.XLSX:
+        tmpdir = tempfile.mkdtemp(suffix='-'+filename_without_ext(path))
+        csvdir = join(tmpdir, 'csv-files')
+        info(f'writing intermediary files to {tmpdir}\n')
+        result = import_xlsx(path, csvdir)
+    else:
+        result = [path]
+    return result
+
+
+def load_tables(schema, in_paths: list) -> Dict[pt.TableId, str]:
+    result = {}
+    info('\nloading table info...')
+    for in_path in in_paths:
+        name = filename_without_ext(in_path)
         table_id = get_sheet_table_id(schema, name)
         if not table_id:
-            print('-- SKIPPING: unable to infer table name')
+            info(f'sheet "{name}": unable to infer table name')
             continue
-        print(f'(table {table_id}) ...')
-        data = {table_id: data}
-        report = _validate_data_ext(schema, data, DataKind.spreadsheet,
-                                    version, on_progress=on_progress)
-        if report.valid():
-            continue
-        write_results(report, outdir, name)
+        result[table_id] = in_path
+    if len(result) == 0:
+        info('\nno tables found. did you specify the correct ODM version?')
+        quit(1)
+    info('found tables:')
+    for table_id in result:
+        info(f'- {table_id}')
+    return result
 
-    print()
-    print(f'output dir: {outdir}')
-    print('done!')
+
+def load_db_data(tables: Dict[pt.TableId, str]) -> dict:
+    info('\nloading data...')
+    result = {}
+    for table_id, path in tables.items():
+        result[table_id] = utils.import_dataset(path)
+    return result
+
+
+# XXX: locals must be disabled to avoid `schema` being dumped to console on an
+# exception (and makeing it unreadable)
+app = typer.Typer(pretty_exceptions_show_locals=False)
+
+
+@app.command()
+def main(data_file: str = typer.Argument(..., help=DATA_FILE_DESC),
+         version: str = typer.Option(default=DEF_VER, help=VERSION_DESC),
+         out: str = typer.Option(default="", help=OUT_DESC),
+         format: Optional[Format] = typer.Option(default=None,
+                                                 help=FORMAT_DESC),
+         verbosity: int = typer.Option(default=2, help=VERB_DESC)):
+
+    out_path = out
+    out_fmt = format
+    in_path = data_file
+    in_fmt = detect_data_format(in_path)
+
+    if not in_fmt:
+        info(f'Invalid data file type for "{os.path.basename(in_path)}". ' +
+             f'Valid file types are {enum_values(DataFormat)}.')
+        quit(1)
+
+    if not out_fmt:
+        if out_path:
+            out_fmt = detect_output_format(out_path)
+            if not out_fmt:
+                out_fmt = Format.TXT
+                info('Unable to infer output format from filename, ' +
+                     f'using {out_fmt}.')
+        else:
+            out_fmt = Format.TXT
+
+    schema_path = get_schema_path(version)
+    schema = utils.import_schema(schema_path)
+
+    if out_path:
+        info(f'writing result to {out_path}\n')
+
+    output = open(out_path, 'w') if out_path else sys.stdout
+    try:
+        info(f'validating "{in_path}"')
+        info(f'using schema "{os.path.basename(schema_path)}"')
+        validation._VERBOSITY = verbosity
+
+        in_paths = preprocess_datafile(in_path, in_fmt)
+        tables = load_tables(schema, in_paths)
+        db_data = load_db_data(tables)
+
+        def validate(data):
+            result = _validate_data_ext(schema, data, DataKind.spreadsheet,
+                                        version, on_progress=on_progress)
+            info()  # newline after progressbar
+
+            # XXX: just in case the validation wrote anything to the console,
+            # to avoid race-conditions with upcoming report output
+            sys.stdout.flush()
+            sys.stderr.flush()
+            return result
+
+        # TODO: we should write continuously to output when the user is
+        # watching in realtime on the terminal, however, stdout can be piped to
+        # somewhere else, so we should detect and take that into account
+        info()
+        is_terminal = out_fmt == Format.TXT and not out_path
+        if is_terminal:
+            for table_id, table_data in db_data.items():
+                report = validate({table_id: table_data})
+                write_txt_report(output, report)
+                info()
+        else:
+            main_report = None
+            for table_id, table_data in db_data.items():
+                report = validate({table_id: table_data})
+                main_report = join_reports(main_report, report)
+            write_report(output, main_report, out_fmt)
+
+        # XXX: report data doesn't have newline at the end, so we need to add
+        # it here
+        info()
+
+        # XXX: flush to prevent race-condition between stdout and stderr, due
+        # to cached writing of the big reports
+        sys.stdout.flush()
+    finally:
+        if out_path:
+            output.close()
+
+    info()
+    if out_path:
+        info(f'result file: {out_path}')
+    info('done!')
 
 
 if __name__ == '__main__':
-    typer.run(main)
+    app()
