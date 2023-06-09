6469db1f4eca656fbc3c071209353e700ece0a06
zargot <zargot.ripley@gmail.com>
2023-06-09 14:06:34 -0400

    work

--- docs/convert.sh
+++ docs/convert.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -e
-set -x
-quarto render ./**/*.qmd --to=ipynb
-find . -name "*.ipynb" -exec sh -c 'pandoc "${0}" -t gfm -o "${0%.ipynb}.md"' {} \;
+# set -x
+script_dir=$(dirname $0)
+files="$script_dir/**/*.qmd"
+count=$(ls $files | wc -l)
+echo "converting $count qmd files to gfm"
+echo "rendering..."
+quarto render $files --no-cache
+echo "converting..."
+find . -name "*.html" -exec sh -c 'pandoc "${0}" -t gfm -o "${0%.ipynb}.md"' {} \;
+echo "done"
