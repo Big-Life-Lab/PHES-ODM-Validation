bdb86a4a26f3d27bb620fe20ceb643d31512069d
zargot <zargot.ripley@gmail.com>
2025-02-03 11:17:56 -0500

    fix inttest pkg install

--- inttests/test-import.sh
+++ inttests/test-import.sh
@@ -7,13 +7,12 @@ set -ex
 
 [[ "$(python --version)" == "Python 3.9."* ]]
 
-repo=$(dirname $(realpath $0))/..
-url="git+file://$repo"
+repodir=$(dirname $(realpath $0))/..
 dir=$(mktemp -d)
 
 cd $dir
 python -m venv .env
 source .env/bin/activate
 
-pip install $url
+pip install $repodir
 python -c "from odm_validation.validation import validate_data"
